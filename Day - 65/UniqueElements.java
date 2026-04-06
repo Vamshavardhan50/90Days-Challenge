0586da2ad945aa00a6444508849665fb2e45f35e
Vamsha vardhan <chintha.vamshavardhan50@gmail.com>
2026-04-06 23:55:50 +0530

    Day-65

---
+++ Day - 65/UniqueElements.java
@@ -0,0 +1,10 @@
+public class UniqueElements {
+  public static void main(String[] args) {
+    int[] arr = { 1, 2, 1, 3, 5, 2 };
+    int ans = 0;
+    for (int i = 0; i < arr.length; i++) {
+      ans = ans ^ arr[i];
+    }
+    System.out.println(ans);
+  }
+}
